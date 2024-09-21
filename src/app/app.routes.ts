88573c58ec52949c58073b46ed9f9cafaa07f923
KevinVC-local <kvcm3h167@gmail.com>
2024-09-20 23:14:41 -0500

    router ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '', loadComponent: () => import('./pages/layout/layout.component').then(m => m.LayoutComponent)},
-    {path: '**', redirectTo: '/'}
+    {path: '**', redirectTo: '/', pathMatch: 'full'}
 ];
